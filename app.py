588a50008208a2988ef62b03738e4f9d3d814a1f
Shreyas Gupta <technology.shreyas@gmail.com>
2018-10-11 09:32:49 +0530

    Fixed error functions

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ def profile(user):
 #Errors
 
 @app.errorhandler(403)
-def page_not_found(e):
+def forbidden(e):
     return render_template('403.html'), 403
 
 @app.errorhandler(404)
@@ -173,7 +173,7 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
+def server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == "__main__":
